f21bdb8cbc0d748612c0866dc87f8d557e2cb22d
rhadiyazgamaputra32 <radiasgama@gmail.com>
2016-10-11 09:24:57 +0700

    praktikum 5
    praktikum 5

---
+++ praktikum5_2.ino
@@ -0,0 +1,13 @@
+void setup() {
+ Serial.begin(115200);
+}
+void loop() {
+ Serial.write(0x44);
+ Serial.write(0x01);
+ Serial.write(0x02);
+ Serial.write(0x03);
+ Serial.write(0x04);
+ 
+ delay(2000);
+} 
+
